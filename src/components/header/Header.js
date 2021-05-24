cae30306687ab1b969198dd542eaafd2f579cb18
Lateef <lateefogunbadejo@yahoo.com>
2021-05-24 21:24:09 +0100

    include logout icon in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './Header.scss';
 
+import logout_icon from '../../assets/icons/logout.svg';
+
 const Header = () => {
   return (
     <header className="header">
       <h3>Shapes</h3>
-      <div>
-        <p>logout</p>
+      <div className="logout-button">
+        <p>Logout</p>
+        <img src={logout_icon} alt="logout icon" />
       </div>
     </header>
   );
